68f982f6c861de7cd7750ff36c6d29a8fc7ccb70
Oyedotun Teslim <54902935+teslima0@users.noreply.github.com>
2022-06-01 21:00:19 +0100

    Update settings.py

--- api/api/settings.py
+++ api/api/settings.py
@@ -125,7 +125,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS=[os.path.join(BASE_DIR,'static'),]
+ if DEBUG:
+        STATICFILES_DIRS = [
+            os.path.join(BASE_DIR, 'static')
+       ]
+    else:
+        STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 MEDIA_URL="/media/"
 MEDIAL_ROOT= os.path.join(BASE_DIR,'media')
 
